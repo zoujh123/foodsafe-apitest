a3f35e2ec0f0728bed9896a64d3c66472bd8c7f4
zhansm <1367721337@qq.com>
2022-07-22 11:22:40 +0800

    区域管理，查列表，增删改区域（区域没有详情页）。

--- utils/ParamOperator.py
+++ utils/ParamOperator.py
@@ -19,3 +19,10 @@ def parseRandParam(jsonString):
     randStr = "%d" % (random.randint(0, 1000))
     jsonString = jsonString.replace("{rand}", randStr)
     return jsonString
+
+def parseRandParamChinese(jsonString):
+    # nowTimestamp = time.time()
+    randStr = random.randint(0x4e00, 0x9fbf)
+    jsonString = jsonString.replace("{rand}", chr(randStr))
+    return jsonString
+
